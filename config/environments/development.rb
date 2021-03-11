2cc2f12799481c17677bb38f122d4b7993410c02
tashi1118 <tech.tashi1118.1991@gmail.com>
2021-03-12 01:47:25 +0900

    yarnのパッケージ整合性チェックでappコンテナが落ちるので一時回避策

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # puma起動時にコンテナが落ちてしまう件の応急処置
+  config.webpacker.check_yarn_integrity = false
 end
