e62290f2ec48dd89ecca78050f887f378817614c
tashi1118 <tech.tashi1118.1991@gmail.com>
2021-03-12 23:25:48 +0900

    @nuxtjs/proxy追加とmodule使用記述

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -35,6 +35,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/proxy'	
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
