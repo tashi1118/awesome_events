82be5a1cbeacc9f6f45e37be2498f3e3f063dac2
tashi1118 <tech.tashi1118.1991@gmail.com>
2021-03-13 15:16:28 +0900

    レイアウト作った

---
+++ frontend/components/TheFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-footer padless>
+      <v-col class="text-center" cols="12">
+        {{ new Date().getFullYear() }} — Vuetify
+      </v-col>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      localAttr: {
+        google: 'https://google.com',
+      },
+    }
+  },
+}
+</script>
+
+<style></style>
---
+++ frontend/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-app-bar flat>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>タイトル</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: 'aaaa',
+    }
+  },
+}
+</script>
+
+<style></style>
