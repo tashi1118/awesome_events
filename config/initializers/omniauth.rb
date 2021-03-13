6331fd2a6d79fd64f7cc6793078ff3669b5fdb0d
tashi1118 <tech.tashi1118.1991@gmail.com>
2021-03-13 18:20:16 +0900

    omniauth.rb作成

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,11 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  if Rails.env.development? || Rails.env.test?
+    provider :github,
+      Rails.application.credentials.github[:client_id],
+      Rails.application.credentials.github[:client_secret]
+  else
+    provider :github,
+      Rails.application.credentials.github[:client_id],
+      Rails.application.credentials.github[:client_secret]
+  end
+end
