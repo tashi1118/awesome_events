3b772573d9a6592c337cef5f9a1b88b3dcc5e5f6
tashi1118 <tech.tashi1118.1991@gmail.com>
2021-03-13 22:16:38 +0900

    omniauth用設定ファイル作成

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  OmniAuth.config.allowed_request_methods = [:get, :post]
   if Rails.env.development? || Rails.env.test?
     provider :github,
       Rails.application.credentials.github[:client_id],
